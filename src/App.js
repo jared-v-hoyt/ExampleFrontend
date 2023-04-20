c893edb6a10082c33d91dd876bf31e2567977180
Jared Hoyt <jared.v.hoyt@gmail.com>
2023-04-20 02:06:31 -0500

    Remove disable_buttons state and parse_product_list function

--- src/App.js
+++ src/App.js
@@ -6,29 +6,21 @@ export default function App() {
 	const [product_list, set_product_list] = useState([]);
 	const product_list_handler = (list) => set_product_list(list);
 
-	const [disabled_buttons, set_disabled_buttons] = useState([
-		false,
-		true,
-		true,
-	]);
-	const disabled_buttons_handler = (button_states) =>
-		set_disabled_buttons(button_states);
+	const [is_loading, set_is_loading] = useState([false, false, false]);
+	const is_loading_handler = (boolean_array) => set_is_loading(boolean_array);
 
-	const [is_loading, set_is_loading] = useState(false);
-	const is_loading_handler = (bool) => set_is_loading(bool);
+	function fetch_order_list() {
+		// TODO: implement
+		return;
+	}
 
-	function parse_product_list(result) {
-		let list = [];
-
-		result.forEach((product) => {
-			list.push(<tr>{product.productName}</tr>);
-		});
-
-		product_list_handler(list);
+	function fetch_order_item_list() {
+		// TODO: implement
+		return;
 	}
 
 	function fetch_product_list() {
-		is_loading_handler(true);
+		is_loading_handler([false, false, true]);
 
 		// TODO: remove timeout. This is only here to demonstrate the CircularProgress component
 		setTimeout(() => {
@@ -39,9 +31,15 @@ export default function App() {
 
 			fetch("http://localhost:5134/products", request_options)
 				.then((response) => response.json())
-				.then((result) => parse_product_list(result))
+				.then((result) => {
+					let list = [];
+					result.forEach((product) =>
+						list.push(<tr>{product.productName}</tr>)
+					);
+					product_list_handler(list);
+				})
 				.catch((error) => console.log("error", error))
-				.finally(is_loading_handler(false));
+				.finally(is_loading_handler([false, false, false]));
 		}, 2000);
 	}
 
@@ -50,28 +48,30 @@ export default function App() {
 			<Box className="button-group-wrapper">
 				<Box className="button-group">
 					<Button
+						disabled={true} // TODO: implement fetch_order_list, then update this line of code
+						onClick={fetch_order_list}
 						variant="contained"
-						disabled={disabled_buttons[0]}
+					>
+						{is_loading[0] ? <CircularProgress size={24} /> : "Get Orders"}
+					</Button>
+					<Button
+						disabled={true} // TODO: implement fetch_order_item_list, then update this line of code
+						onClick={fetch_order_item_list}
+						variant="contained"
+					>
+						{is_loading[1] ? <CircularProgress size={24} /> : "Get OrderItems"}
+					</Button>
+					<Button
+						disabled={is_loading[2]}
 						onClick={fetch_product_list}
-					>
-						Get Products
-					</Button>
-					<Button
 						variant="contained"
-						disabled={disabled_buttons[1]}
 					>
-						Get OrderItems
-					</Button>
-					<Button
-						variant="contained"
-						disabled={disabled_buttons[2]}
-					>
-						Get Orders
+						{is_loading[2] ? <CircularProgress size={24} /> : "Get Products"}
 					</Button>
 				</Box>
 			</Box>
 			<Box className="response-wrapper">
-				{is_loading ? <CircularProgress /> : <table>{product_list}</table>}
+				<table>{product_list}</table>
 			</Box>
 		</Box>
 	);
